f1aa1276e2a71450fec340b389cd367520a707df
batoolalshareef <batoolalshareef@gmail.com>
2022-09-24 13:02:15 +0300

    bbbb

--- src/linksInSelenium/MyLinksClass.java
+++ src/linksInSelenium/MyLinksClass.java
@@ -1,10 +1,17 @@
 package linksInSelenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyLinksClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //hello
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
 	}
 
 }
